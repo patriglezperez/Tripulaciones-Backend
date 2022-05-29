f377464effc4d7dd967f1ac74b0374b49e6da686
Sebastian M <ra3gan1991@gmail.com>
2022-05-29 13:56:17 +0200

    Implemented FirebaseSDK with example

--- controllers/prueba.js
+++ controllers/prueba.js
@@ -1,1 +1,6 @@
-//example to go up
+async function prueba(req, res) {
+  req.user?console.log(req.user):console.log('no user');
+    res.status(200).json({message:"ok"});  
+  }
+  
+  module.exports = prueba;
