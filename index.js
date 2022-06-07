fc584fdc82963af41b9262a33a3000d83d076fc4
josemsb92 <josemsb92@gmail.com>
2022-06-07 11:40:16 +0200

    imported conection to db

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 const authentication = require("./middlewares/authentication");
+require("./config/dbConfig");
 port = 3003;
 
-app.use(cors())
+app.use(cors());
 app.use(require("express").json());
 //All routes starting with /api verify that they receive a valid token in the header.
-app.use("/api",authentication, require("./routes"));
+app.use("/api", authentication, require("./routes"));
 //Routes without /api does not require a token.
 app.use("/", require("./routes"));
 
