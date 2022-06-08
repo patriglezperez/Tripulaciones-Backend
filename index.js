5a34509a3d16aca4631968628da5d3c2300bcbe9
josemsb92 <josemsb92@gmail.com>
2022-06-08 18:17:32 +0200

    config.js done and firs set up for connection test

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const authentication = require("./middlewares/authentication");
-require("./config/dbConfig");
 port = 3003;
 
+const { sequelize } = require("./models");
 app.use(cors());
 app.use(require("express").json());
 //All routes starting with /api verify that they receive a valid token in the header.
@@ -15,4 +15,10 @@ app.use("/", require("./routes"));
 
 app.listen(port, () => {
   console.log(`Application running on port ${port}`);
+  try {
+    sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 });
