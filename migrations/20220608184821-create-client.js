d730cc9331056ba6f6580e28ce1fe006f863c009
josemsb92 <josemsb92@gmail.com>
2022-06-12 20:11:53 +0200

    client_image field deleted from model

--- migrations/20220608184821-create-client.js
+++ migrations/20220608184821-create-client.js
@@ -17,9 +17,6 @@ module.exports = {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      image_client: {
-        type: DataTypes.STRING,
-      },
       client_email: {
         type: DataTypes.STRING,
         allowNull: false,
